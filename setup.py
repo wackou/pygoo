3aeccd1128fcd05cafd9072fb98853eb5326f005
Nicolas Wack <wackou@gmail.com>
2010-05-10 23:35:46 +0200

    initial directory structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '0.1-dev'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name = 'pygoo',
+      version = version,
+      description = "PyGoo is an Object-Graph mapper, similar to SQLAlchemy but for graph DBs",
+      long_description = README + '\n\n' + NEWS,
+      classifiers = [], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords = 'pygoo object graph mapper',
+      author = 'Nicolas Wack',
+      author_email = 'wackou@gmail.com',
+      url = 'http://gitorious.org/pygoo',
+      license = 'GPLv3',
+      packages = find_packages(exclude = [ 'ez_setup', 'examples', 'tests' ]),
+      include_package_data = True,
+      zip_safe = False,
+      install_requires = install_requires,
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
